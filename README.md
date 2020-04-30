f251455e61a6afb81a592165a2906709ed1f411d
Jase Kirby <jase.kirby@blackboard.com>
2020-04-30 09:58:51 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Terragrunt Install GitHub Action
+
+This action takes in a terragrunt and terraform verion, downloads the tools, adds them to PATH, and caches them for reuse.
+
+## Inputs
+
+### `terragrunt-version`
+
+**Required** Terragrunt version.
+
+### `terraform-version`
+
+**Required** Terraform version.
+
+## Example Usage
+
+```yaml
+uses: blackboard-innersource/gh-action-terragrunt-install@v1
+with:
+  terraform-version: "0.1.0"
+  terraform-version: "0.1.0"
+```
---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import * as core from "@actions/core";
+import * as tc from "@actions/tool-cache"
+import * as io from "@actions/io";
+import * as path from "path";
+
+async function run(): Promise<void> {
+  try {
+    const terragruntVersion: string = core.getInput("terragrunt-version");
+    core.debug("terragrunt version " + terragruntVersion);
+    const terraformVersion: string = core.getInput("terraform-version");
+    core.debug("terraform version " + terraformVersion);
+
+    const binPath = "bin";
+    io.mkdirP("bin");
+
+    const tgBinPath = path.join(binPath, "terragrunt");
+    await tc.downloadTool(`https://github.com/gruntwork-io/terragrunt/releases/download/v${terragruntVersion}/terragrunt_linux_amd64`,
+      tgBinPath
+    );
+    tc.cacheFile(binPath, "terragrunt", "terragrunt", terragruntVersion);
+
+    const terraformBinPath = path.join(binPath, "terraform");
+    const tfDownloadPath = await tc.downloadTool(`https://releases.hashicorp.com/terraform/${terraformVersion}/terraform_${terraformVersion}_linux_amd64.zip`)
+    await tc.extractZip(tfDownloadPath, binPath);
+    tc.cacheFile(terraformBinPath, "terraform", "terraform", terraformVersion);
+    io.rmRF(tfDownloadPath);
+    
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()
